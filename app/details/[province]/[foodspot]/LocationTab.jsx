fc8484a7ed852ee9817f902e4548d5e8b3ad3ad9
Kevin Jake <kevin_jake_04@yahoo.com>
2023-06-01 23:25:12 +0800

    add LocationTab component

---
+++ app/details/[province]/[foodspot]/LocationTab.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// TODO: Add location function
+const LocationTab = () => {
+  // URL https://www.google.com/maps/search/14.332432142736307,+121.05022358366557
+  return <div>LocationTab</div>;
+};
+
+export default LocationTab;
