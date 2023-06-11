ef088deab57176f6afcfa6079fcfc4b49fd997c4
Kevin Jake <kevin_jake_04@yahoo.com>
2023-06-11 21:47:18 +0800

    update MIT licensing

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-wh-900 text-wh-50 py-10 px-10">
       <div className="w-full flex flex-col justify-center items-center mx-auto max-w-screen-xl p-4">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023 Appetite Lover. All Rights Reserved.{" "}
+          © 2023 Appetite Lover. The source code is licensed MIT.
         </span>
         <span className="text-sm flex justify-center items-center gap-1 text-gray-500 sm:text-center dark:text-gray-400">
           Developed using <SiAppwrite color="gray" /> Appwrite.
